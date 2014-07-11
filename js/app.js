fbe8f7cc7f5f2224128bc1fb84a285e89c7105fd
Bruno Lima <brunolimawd@gmail.com>
2014-07-11 14:37:26 -0300

    Style complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
-// DOMContentLoaded is fired once the document has been loaded and parsed,
-// but without waiting for other external resources to load (css/images/etc)
-// That makes the app more responsive and perceived as faster.
-// https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
 window.addEventListener('DOMContentLoaded', function() {
 
-  // We'll ask the browser to use strict code to help us catch errors earlier.
-  // https://developer.mozilla.org/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode
   'use strict';
 
   var translate = navigator.mozL10n.get;
@@ -24,168 +18,163 @@ window.addEventListener('DOMContentLoaded', function() {
   // So we'll tell it to let us know once it's ready.
   navigator.mozL10n.once(start);
 
-  // ---
 
+  // Start App
   function start() {
-    //geoFindMe();
+    geoFindMe();
+    
+    // Display Control msg
+    results.className = 'hidden';
+    errorMsg.className = 'hidden';
+  };
+
+  // Show error msg
+  function showError(text) {
+    errorMsg.textContent = text;
+    results.className = 'hidden';
+    errorMsg.className = '';
   }
 
+  // Get geolocation
   function geoFindMe() {
-    if (!navigator.geolocation){
-      errorMsg.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-      return;
-    }
-
     function success(position) {
       latitude  = position.coords.latitude;
       longitude = position.coords.longitude;
+
+      // Init search
       search();
     };
 
     function error() {
-      errorMsg.innerHTML = "Unable to retrieve your location";
+      showError(translate('unable_location'));
     };
 
-    results.innerHTML = "<p>Locating…</p>";
+    // Show status to user
+    results.textContent = translate('locating');
+    results.className = 'searching';
+    errorMsg.className = 'hidden';
 
     navigator.geolocation.getCurrentPosition(success, error);
-  }
+  };
 
 
+  // Listening reload button
   buttonFindMe.addEventListener('click', function(e) {
       geoFindMe();
   }, false);
 
-  function getRelativeTime(time) { 
 
+  // Convert created time to time ago
+  function getRelativeTime(time) {
     var parsed_date = new Date (time * 1000);
-    var relative_to = (arguments.length > 1) ? arguments[1] : new Date(); // Defines relative to what ... default is now
+    // Defines relative to what ... default is now
+    var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
     var delta = parseInt((relative_to.getTime()-parsed_date)/1000);
     delta=(delta<2)?2:delta;
-
-    var r = '';
+    var timeAgo = '';
 
     // Parse delta
     if (delta < 60) {
-      r = delta + ' seconds ago';
+      timeAgo = delta + ' ' + translate('seconds_ago');
     } else if(delta < 120) {
-      r = 'a minute ago';
+      timeAgo = translate('a_minute_ago');
     } else if(delta < (45*60)) {
-      r = (parseInt(delta / 60, 10)).toString() + ' minutes ago';
+      timeAgo = (parseInt(delta / 60, 10)).toString() + ' ' + translate('minutes_ago');
     } else if(delta < (2*60*60)) {
-      r = 'an hour ago';
+      timeAgo = translate('an_hour_ago');
     } else if(delta < (24*60*60)) {
-      r = '' + (parseInt(delta / 3600, 10)).toString() + ' hours ago';
+      timeAgo = '' + (parseInt(delta / 3600, 10)).toString() + ' ' + translate('hours_ago');
     } else if(delta < (48*60*60)) {
-      r = 'a day ago';
+      timeAgo = translate('a_day_ago');
     } else {
-      r = (parseInt(delta / 86400, 10)).toString() + ' days ago';
+      timeAgo = (parseInt(delta / 86400, 10)).toString() + ' ' + translate('days_ago');
     }
 
-    return r;
-
+    return timeAgo;
   };
 
 
-  function sharePhoto(urlInstagram){
+  // Web activity for share url photo
+  function sharePhoto(photoUrl){
     var activityShare = new MozActivity({
-      // Ask for the "share" activity
-      name: "share",
-
-      // Provide the data required by the filters of the activity
+      name: 'share',
       data: {
-        type: "url",
-        url: urlInstagram
+        type: 'url',
+        url: photoUrl
       }
     });
 
     activityShare.onsuccess = function() {
-      console.log("ok");
+      console.log('Success');
     };
 
     activityShare.onerror = function() {
       console.log(this.error);
     };
-  };
-  
+  };  
 
 
   // Get photos by location on Instagram
   function search() {
-
     // Are we searching already? Then stop that search
     if(request && request.abort) {
       request.abort();
     }
 
-
+    // Show status to user
     results.textContent = translate('searching');
+    results.className = 'searching';
+    errorMsg.className = 'hidden';
 
-    // We will be using the 'hidden' attribute throughout the app rather than a
-    // 'hidden' CSS class because it enhances accessibility.
-    // See: http://www.whatwg.org/specs/web-apps/current-work/multipage/editing.html#the-hidden-attribute
-    results.hidden = false;
-    errorMsg.hidden = true;
-
-
+    // Creat url for request
     var term = '&lat=' + latitude + '&lng=' + longitude + '&distance=' + distance;
-
     var url = apiURL + term;
 
-    // If you don't set the mozSystem option, you'll get CORS errors (Cross Origin Resource Sharing)
-    // You can read more about CORS here: https://developer.mozilla.org/docs/HTTP/Access_control_CORS
     request = new XMLHttpRequest({ mozSystem: true });
-
     request.open('get', url, true);
     request.responseType = 'json';
 
-    // We're setting some handlers here for dealing with both error and
-    // data received. We could just declare the functions here, but they are in
-    // separate functions so that search() is shorter, and more readable.
+    // Return for request
     request.addEventListener('error', onRequestError);
     request.addEventListener('load', onRequestLoad);
 
     request.send();
-  }
+  };
 
 
+  // Request error msg
   function onRequestError() {
-    var errorMessage = request.error;
-      if(!errorMessage) {
-        errorMessage = translate('searching_error');
-      }
-      showError(errorMessage);
-  }
+    console.log(request.error);    
+    showError(translate('searching_error'));
+  };
 
-
+  // Request and load complete
   function onRequestLoad() {
     var response = request.response;
 
+    // Check response
     if(response === null) {
       showError(translate('searching_error'));
       return;
     }
 
+    // Clear results content
     results.textContent = '';
+    results.className = '';
 
     var photos = response.data;
     
     if(photos.length === 0) {
-
-      var p = document.createElement('p');
-      p.textContent = translate('search_no_results');
-      results.appendChild(p);
-
+      results.textContent = translate('search_no_results');
+      results.className = 'searching';
+      errorMsg.className = 'hidden';
     } else {
-
       var listPhotos = document.createElement('ul');
       results.appendChild(listPhotos);
 
       photos.forEach(function(item) {
-
         // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
         // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
-
         var liPhoto = document.createElement('li');
         var header = document.createElement('header');
         var profileAvatar = document.createElement('img');
@@ -198,6 +187,7 @@ window.addEventListener('DOMContentLoaded', function() {
         var liShare = document.createElement('li');
         var buttonSahre = document.createElement('button');
 
+        // Set values
         liPhoto.className = 'photo';
         profileAvatar.src = item.user.profile_picture;
         profileAvatar.alt = item.user.username;
@@ -214,6 +204,7 @@ window.addEventListener('DOMContentLoaded', function() {
         buttonSahre.value = item.link;
         buttonSahre.textContent = 'Share';
 
+        // Creat photo template 
         header.appendChild(profileAvatar);
         header.appendChild(profileName);
         header.appendChild(timeAgo);
@@ -230,27 +221,17 @@ window.addEventListener('DOMContentLoaded', function() {
 
         listPhotos.appendChild(liPhoto);
 
-        liPhoto.innerHTML=emoji.a(liPhoto.innerHTML);
+        // Convert emoji
+        profileName.innerHTML = emoji.a(profileName.innerHTML);
+        photoCaption.innerHTML = emoji.a(photoCaption.innerHTML);
 
+        // Listening share button
         buttonSahre.addEventListener('click', function(e) {
             sharePhoto(this.value);
         }, false);
-
       });
-
     }
 
-    // And once we have all the content in place, we can show it.
-    results.hidden = false;
-
-  }
-
-
-  function showError(text) {
-    errorMsg.textContent = text;
-    errorMsg.hidden = false;
-    results.hidden = true;
-  }
-
-
+    results.className = '';
+  };
 });
